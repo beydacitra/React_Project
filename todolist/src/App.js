a0c397e50ec1aa4e018e0bd491ff3cfcbf67e42a
JUNAID KHAN <junaidmushtaq988@gmail.com>
2024-07-13 12:42:55 +0500

    inital commit

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -4,27 +4,35 @@ const App = () => {
   const [input , setInput] = useState('');
   const [items , setItems] = useState([]);
 
-  const handlebutton = ( ) => {
-    if(input.trim()) {
-      setItems([...items , input]);
+  const handleinput = () => {
+    if(input.trim()){
+      setItems([...items , { text: input, completed: false }])
     };
-    setInput('')
+      setInput('');
+  };
+  const markcomplete = (index) => {
+    const newitems = ([...items]);
+    newitems[index].completed = true ;
+    setItems(newitems)
   }
   return (
-    <div className='text-center bg-dark text-white w-screen h-screen'>
-      <h2>StateMangement</h2>
-       <input 
-       className='border border-black rounded-xl shadow-md text-black m-1 p-1'
-       value={input}
-       type='text'
-       onChange={(e) => {setInput(e.target.value)}}
-       />
-       <button onClick={handlebutton} className='btn font-bold text-white' >click me</button >
-       <ul>
+    <div>
+      <input 
+      type='text'
+      value={input}
+      onChange={(e) => setInput(e.target.value)}
+      />
+      <button onClick={handleinput}>click</button>
+      <ul>
         {items.map((item , index) => (
-          <li key={index} > {item} </li>
+          <li key={index} style={{textDecoration : item.completed ? 'line-through' : 'none'}}>
+            {item.text} 
+            {!item.completed && (
+              <button onClick={() => markcomplete(index)}>Mark Completed</button>
+            )}
+            </li>
         ))}
-       </ul>
+      </ul>
     </div>
   )
 }
