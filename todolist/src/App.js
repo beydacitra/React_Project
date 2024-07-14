ba00bb6bcbfa53a93b42135d36e04d48bf1f2e85
JUNAID KHAN <junaidmushtaq988@gmail.com>
2024-07-14 18:04:43 +0500

    Added some new errr

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -6,13 +6,22 @@ const App = () => {
 
   const handlebutton = ( ) => {
     if(input.trim()) {
-      setItems([...items , input]);
+      setItems([...items , {text:input , font : false}]);
     };
     setInput('')
+  };
+  const handlechange = (index) => {
+    const newitem = items.map((item , idx) => {
+      if(index === item){
+        return{...item , font : !item.font }
+      }
+      return items;
+    });
+    setItems(newitem)
   }
   return (
     <div className='text-center bg-dark text-white w-screen h-screen'>
-      <h2>StateMangement</h2>
+      <h2>Mangement</h2>
        <input 
        className='border border-black rounded-xl shadow-md text-black m-1 p-1'
        value={input}
@@ -22,7 +31,12 @@ const App = () => {
        <button onClick={handlebutton} className='btn font-bold text-white' >click me</button >
        <ul>
         {items.map((item , index) => (
-          <li key={index} > {item} </li>
+          <li key={index} >
+            <span style={{fontWeight : item.font ? 'bolder' : 'initial'}} >
+              {item.text}
+            </span>
+              <button onClick={handlechange(index)}>font</button>
+          </li>
         ))}
       </ul>
     </div>
