a97da81516b053ede9af4312eca0d477a506ae00
JUNAID KHAN <junaidmushtaq988@gmail.com>
2024-07-21 20:48:26 +0500

    initial commit

--- Qamartea.pk/src/Components/About.jsx
+++ Qamartea.pk/src/Components/About.jsx
@@ -7,7 +7,7 @@ export default function About() {
             <div className="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
                 <div className="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
                     <div className="md:5/12 lg:w-5/12">
-                        <img
+                           <img
                             src="https://tailus.io/sources/blocks/left-image/preview/images/startup.png"
                             alt="image"
                         />
