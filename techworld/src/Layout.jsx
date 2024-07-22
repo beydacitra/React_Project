337ebc9549a0e20410ba7f702336b3643d3ca9d6
JUNAID <junaidmushtaq988@gmail.com>
2024-07-22 19:51:27 +0500

    Initial commit

---
+++ techworld/src/Layout.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import { Outlet } from 'react-router-dom';
+
+import Footer from './Components/Footer/Footer';
+import Header from './Components/Header/Header';
+
+const Layout = () => {
+  return (
+    <div>
+        <Header/>
+          <Outlet/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default Layout;
