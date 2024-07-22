0d19bdd6431e83d788eb98fa55961b6bd36bf52b
JUNAID KHAN <junaidmushtaq988@gmail.com>
2024-07-22 12:25:40 +0500

    I Don't know what happend ??

--- Qamartea.pk/src/Components/Navbar.jsx
+++ Qamartea.pk/src/Components/Navbar.jsx
@@ -96,7 +96,7 @@ const Header = () => {
               <NavLink
                 to="/about"
                 activeClassName="text-orange-600"
-                className="block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0"
+                className="block borser py-2 pr-4 pl-3 duration-200 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0"
                 onClick={toggleMenu}
               >
                 About
