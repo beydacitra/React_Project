1bcbe36479a23f35e720b2fc2b9ef51af0d40b24
junaid khan <junaidmushtaq988@gmail.com>
2024-07-22 21:31:29 +0500

    inital

--- techworld/src/Layout.jsx
+++ techworld/src/Layout.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 
-import { Outlet } from 'react-router-dom';
-
 import Footer from './Components/Footer/Footer';
 import Header from './Components/Header/Header';
 
@@ -9,7 +7,7 @@ const Layout = () => {
   return (
     <div>
         <Header/>
-          <Outlet/>
+        
         <Footer/>
     </div>
   )
