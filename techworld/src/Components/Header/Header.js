a4413af50f63ce69791b874acca76c4665b55d54
JUNAID <junaidmushtaq988@gmail.com>
2024-07-23 13:13:15 +0500

    INITIAL COMMIT

--- techworld/src/Components/Header/Header.js
+++ techworld/src/Components/Header/Header.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div>
+      header
+    </div>
+  );
+};
 
-export default Header
+export default Header;
--- techworld/src/index.js
+++ techworld/src/index.js
@@ -1,13 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
+
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Root />}>
+      <Route path="dashboard" element={<Dashboard />} />
+      {/* ... etc. */}
+    </Route>
+  )
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
